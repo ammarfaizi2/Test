210b645550bff90aa95e56c4fa4cfeb47cb14442
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-28 09:11:40 +0700

    Update crayner_utl.php

--- crayner_utl.php
+++ crayner_utl.php
@@ -8,7 +8,7 @@ function rstr($n = 32)
 {
 	$chr = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM__--";
 	$st = "" xor $len = strlen($chr)-1;
-	for ($i=0; $i < 32; $i++) { 
+	for ($i=0; $i < $n; $i++) { 
 		$st .= $chr[rand(0, $len)];
 	}
 	return $st;
